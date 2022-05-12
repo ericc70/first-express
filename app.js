f3340b541040b7b2bf2675234d07b7ec9d46e5ee
ericc70 <eric070@net-c.fr>
2022-05-12 19:42:46 +0200

    user auth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("./connectdb");
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 
 const app = express()
@@ -18,4 +19,6 @@ app.use((req, res, next) => {
   next();
 });
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 module.exports = app;
