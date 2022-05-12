0001f89655230fd1fe38a928ad6280a5ba50dddb
ericc70 <eric070@net-c.fr>
2022-05-12 12:58:50 +0200

    get one thing

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const mongoose = require('./connectdb')
+const express = require("express");
+const mongoose = require("./connectdb");
 
- const Thing = require('./models/Thing')
+const Thing = require("./models/Thing");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 // app.use( (req, res, next) =>{
 //     console.log('requete recue')
 //     next()
@@ -24,27 +24,40 @@ app.use(express.json())
 // })
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  })
-  app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id
-    const thing = new Thing({
-      ...req.body
-    })
-    // res.status(201).json({
-    //   message: 'Objet créé !'
-  thing.save()
-  .then(() =>res.status(201).json({ message: "object enregistrer" }) )
-  .catch( error => res.status(400).json({error}))
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.post("/api/stuff", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body,
   });
-app.get('/api/stuff', (req, res, next) => {
-   
-Thing.find()
-.then( things => res.status(200).json(things))
-.catch( error => res.status(400).json({error}))
+  // res.status(201).json({
+  //   message: 'Objet créé !'
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "object enregistrer" }))
+    .catch((error) => res.status(400).json({ error }));
+});
 
-  });
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({_id: req.params.id })
+  .then( thing => res.status(200).json(thing))
+  .catch( error => res.status(400).json({error}))
+})
+
+app.get("/api/stuff", (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
+});
 module.exports = app;
